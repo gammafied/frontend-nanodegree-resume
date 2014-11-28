ec6d23c0deb931cc9c50332d66392e16a446de03
Marlina Ubuntu McKay <gammafied@gmail.com>
2014-11-27 21:13:29 -0500

    Learn about append and replace in jQuery

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,1 +1,15 @@
-  $("#main").append([Warren Piece]);
+  $("#main").append(["Warren Piece"]);
+  var firstName = "marlina";
+  var awesomeThoughts = "I am Freddy and I am AWESOME!";
+  var firstRole = "Web Guru";
+  
+  var funThoughts = awesomeThoughts.replace("AWESOME", firstName);
+  $("#main").append("<br />", funThoughts);
+  
+  var formattedName = HTMLheaderName.replace("%data%", firstName);  
+  $("#header").prepend(formattedName);
+  
+  var formattedRole = HTMLheaderRole.replace("%data%", firstRole);
+  $("#header").append(formattedRole);
+      
+      
